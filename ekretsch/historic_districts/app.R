99ef8ab7619daa1377ae523738372afd1dcfd544
ekretsch <ekretschmer.sign.up.email@gmail.com>
2026-06-24 08:42:42 -0400

    got the shiny app to do something!

--- ekretsch/historic_districts/app.R
+++ ekretsch/historic_districts/app.R
@@ -7,15 +7,62 @@ library(tigris)
 
 library(tidyverse)
 
+# Getting data n such -----
+historic_districts <- read_csv("../../data/Historic Districts/historic_districts_clean4.csv")
+areas <- read_csv("../../data/Historic Districts/us_areas_cleaned.csv")
+
+by_state <- historic_districts %>% 
+  group_by(state) %>% 
+  summarise(total_acreage = sum(acreage_of_property, na.rm=TRUE),
+            total_num_districts = n())
+
+# Get state geometries
+states_sf <- tigris::states(cb = TRUE, resolution = "20m")
+
+# Join data to shapefile
+map_data <- states_sf %>% 
+  left_join(by_state, by = c("NAME" = "state")) %>% 
+  left_join(areas, by = c("NAME" = "state_or_territory")) %>% 
+  mutate(total_acreage_hd = total_acreage) %>% 
+  select(-total_acreage)
+
+# Standardizing (historic district acreage by total state land acreage)
+map_data <- map_data %>% 
+  mutate(standardized_hd_acreage = total_acreage_hd/land_area_acres) 
+
+# Color palette (UNSURE IF THIS SHOULD GO HERE OR LaTER)
+my_palette <- colorNumeric(
+  palette = "viridis", 
+  domain = map_data$standardized_hd_acreage,
+  na.color = "transparent"
+)
+
 # Define UI -----
-ui <- page_sidebar( # og fluidpage
+ui <- fluidPage(
   title = "Historic Districts",
-  sidebar = sidebar("Sidebar", position = "right"),
-  "Main contents"
+  leafletOutput("map")
 )
 
 # Define server logic -----
 server <- function(input, output) {
+  output$map <- renderLeaflet({
+    leaflet(map_data) %>% 
+      addProviderTiles("CartoDB.Positron") %>% 
+      addPolygons(
+        fillColor = ~my_palette(standardized_hd_acreage),
+        fillOpacity = .75,
+        color = "white", # border color
+        weight = 1,
+        smoothFactor = 0.5, # slightly crisper borders -- default is 1 (higher values > more simplification > jaggier borders but shorter rendering)
+        # add the highlight/hover and tooltip things
+      ) %>% 
+      addLegend(
+        pal = my_palette,
+        value = map_data$standardized_hd_acreage, # same as values   = ~total_num_districts
+        position = "bottomright",
+        title = "Hist. Dist. area/state land area"
+      )
+  })
 }
 
 # Run the app -----
