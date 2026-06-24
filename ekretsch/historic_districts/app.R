5fb21817cab04d10f37b8c20747ad20854e219ac
ekretsch <ekretschmer.sign.up.email@gmail.com>
2026-06-24 08:22:33 -0400

    starting my shiny app

---
+++ ekretsch/historic_districts/app.R
@@ -0,0 +1,67 @@
+# Packages
+library(shiny)
+library(bslib)
+
+library(leaflet)
+library(tigris)
+
+library(tidyverse)
+
+# Define UI -----
+ui <- page_sidebar( # og fluidpage
+  title = "Historic Districts",
+  sidebar = sidebar("Sidebar", position = "right"),
+  "Main contents"
+)
+
+# Define server logic -----
+server <- function(input, output) {
+}
+
+# Run the app -----
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+# Old faithful
+
+# Define UI for application that draws a histogram
+# ui <- fluidPage(
+#   
+#   # Application title
+#   titlePanel("Old Faithful Geyser Data"),
+#   
+#   # Sidebar with a slider input for number of bins 
+#   sidebarLayout(
+#     sidebarPanel(
+#       sliderInput("bins",
+#                   "Number of bins:",
+#                   min = 1,
+#                   max = 50,
+#                   value = 30)
+#     ),
+#     
+#     # Show a plot of the generated distribution
+#     mainPanel(
+#       plotOutput("distPlot")
+#     )
+#   )
+# )
+# 
+# # Define server logic required to draw a histogram
+# server <- function(input, output) {
+#   
+#   output$distPlot <- renderPlot({
+#     # generate bins based on input$bins from ui.R
+#     x    <- faithful[, 2]
+#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+#     
+#     # draw the histogram with the specified number of bins
+#     hist(x, breaks = bins, col = 'darkgray', border = 'white',
+#          xlab = 'Waiting time to next eruption (in mins)',
+#          main = 'Histogram of waiting times')
+#   })
+# }
+# 
